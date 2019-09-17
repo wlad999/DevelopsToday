c702d743417900eef18a9a6616632d64127f639a
ваше имяwlad999 <wladmail25@gmeil.com>
2019-09-17 15:21:15 +0300

    Updates

---
+++ precache-manifest.45c6c2ee14e0b7c6895ae44699a8f189.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec8b6d1fa0152d1ed49f162d0eb90a0",
+    "url": "/DevelopsToday/index.html"
+  },
+  {
+    "revision": "df7169b4849048ee9684",
+    "url": "/DevelopsToday/static/css/main.2581229b.chunk.css"
+  },
+  {
+    "revision": "065e4c633f46e7204ee3",
+    "url": "/DevelopsToday/static/js/2.adb82095.chunk.js"
+  },
+  {
+    "revision": "df7169b4849048ee9684",
+    "url": "/DevelopsToday/static/js/main.3c8b53d8.chunk.js"
+  },
+  {
+    "revision": "9988a04ae6dc7e61a1ef",
+    "url": "/DevelopsToday/static/js/runtime~main.0965de31.js"
+  },
+  {
+    "revision": "a6f66922c08709037ca326697513dbb6",
+    "url": "/DevelopsToday/static/media/slider0.a6f66922.jpg"
+  },
+  {
+    "revision": "67b5507da87e41f4974fda2110a88194",
+    "url": "/DevelopsToday/static/media/slider2.67b5507d.jpg"
+  }
+]);
